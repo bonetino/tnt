2144bddfab6223cc3d177329c6696ade8eb7f70b
Sinisa Botic <sinisa@bonet.hr>
2016-08-07 00:23:43 +0200

    inicijalni

---
+++ js/main.js
@@ -0,0 +1,280 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// $('body').toggleClass('fh5co-offcanvas');
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});	
+
+	}
+
+	
+
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+
+	// Document on load.
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		
+
+	});
+
+
+}());
+
+// Newsletter forma
+
+jQuery(document).ready(function($){
+	var messages = $('div[data-type="message"]');
+	//check if user updates the email field
+	$('.cd-form .cd-email').keyup(function(event){	
+		//check if user has pressed the enter button (event.which == 13)
+		if(event.which!= 13) {
+			//if not..
+			//hide messages and loading bar 
+			messages.removeClass('slide-in is-visible');
+			$('.cd-form').removeClass('is-submitted').find('.cd-loading').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+		}
+
+		var emailInput = $(this),
+			insertedEmail = emailInput.val(),
+			atPosition = insertedEmail.indexOf("@");
+	    	dotPosition = insertedEmail.lastIndexOf(".");
+	    //check if user has inserted a "@" and a dot
+	    if (atPosition< 1 || dotPosition<atPosition+2 ) {
+	    	//if he hasn't..
+	    	//hide the submit button
+	    	$('.cd-form').removeClass('is-active').find('.cd-loading').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+	    } else {
+	    	//if he has..
+	    	//show the submit button
+	    	$('.cd-form').addClass('is-active');
+	    }
+	});
+
+	//backspace doesn't fire the keyup event in android mobile
+	//so we check if the email input is focused to hide messages and loading bar 
+	$('.cd-form .cd-email').on('focus', function(){
+		messages.removeClass('slide-in is-visible');
+		$('.cd-form').removeClass('is-submitted').find('.cd-loading').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+	});	
+
+	//you should replace this part with your ajax function
+	$('.cd-submit').on('click', function(event){
+		if($('.cd-form').hasClass('is-active')) {
+			event.preventDefault();
+			//show the loading bar and the corrisponding message
+			$('.cd-form').addClass('is-submitted').find('.cd-loading').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				showMessage();
+			});
+
+			//if transitions are not supported - show messages
+			if($('html').hasClass('no-csstransitions')) {
+				showMessage();
+			}
+		}
+	});
+
+	function showMessage() {
+		if( $('#cd-success').is(':checked') ) {
+			$('.cd-response-success').addClass('slide-in');
+		} else if ( $('#cd-error').is(':checked') ) {
+			$('.cd-response-error').addClass('is-visible');
+		} else {
+			$('.cd-response-notification').addClass('is-visible');
+		}
+	}
+
+	//placeholder fallback (i.e. IE9)
+	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+		  	}
+		}).blur(function() {
+		 	var input = $(this);
+		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+		  	}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+		  	$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+			 		input.val('');
+				}
+		  	})
+		});
+	}
+});
+
+
+(function ($) {
+   $('.btn-default').on('click', function (e) {
+      e.preventDefault();
+      var obj = $(this);
+
+      obj.addClass('active');
+
+      setTimeout(function () {
+         obj.removeClass('active');
+      }, 1500);
+
+      // Više partnera
+      if (obj.hasClass('btn-load-partneri')) {
+         if (!$('.load-partneri-box').hasClass('open')) {
+            $('.load-partneri-box').slideToggle(1000);
+            setTimeout (function () {
+               $('.load-partneri-box').addClass('open');
+            }, 700);
+         }
+      }
+
+      // Load nagradnih igara
+      if (obj.hasClass('btn-load-nagradna-igra')) {
+         if (!$('.load-nagrade-box').hasClass('open')) {
+            $('.load-nagrade-box').slideToggle(1000);
+            setTimeout (function () {
+               $('.load-nagrade-box').addClass('open');
+            }, 700);
+         }
+      }
+   });
+
+   // Fade In Page
+   $(document).ready(function () {
+      setTimeout(function () {
+         $('body').addClass('dom-ready');
+      }, 300);
+   });
+})(jQuery);
+
+
+//opsirnije za emisije
+$(document).ready(function(){
+    $('.program a').on('click',function(){
+        var aID = $(this).attr('href');
+        var elem = $(''+aID).html();
+        
+        $('.target').html(elem);
+    });
+});
---
+++ program.php
@@ -0,0 +1,221 @@
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>TNT radio | Najbolji radio u regiji</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="TNT radio" />
+	<meta name="keywords" content="radio, tnt, eter, oglasavanje, tnt grupa" />
+	<meta name="author" content="Insideo" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<header id="fh5co-header-section">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<h1 id="brand-logo"><a href="index.html"><img src="images/logo.svg"></a></h1>
+							<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li class="active"><a href="index.html">Home</a></li>								
+								<li><a href="#">O nama</a></li>
+								<li><a href="#">Oglašavanje</a></li>		
+								<li><a href="#">Program</a></li>
+								<li><a href="#">Osvoji</a></li>
+								<li><a href="kontakt.html">Kontakt</a></li>
+								<li>
+									<a href="#" class="top-online red" type="button">Slušaj online</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+			
+		</div>
+		
+
+		<div class="fh5co-hero fh5co-hero-2">
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5">
+					<div class="container">
+						<div class="only-text animate-box">
+								<h2>Trebate produkciju videa ili spota?</h2>
+								<span>Imate mogućnost odabrati što ćemo puštati. Zatražite pjesmu, ocijenite je i pomozite svom favoritu da bude na vrhu top liste</span>
+								<form class="destinations-form">
+			            <div class="input-line">
+			              <input type="text" name="destination" value="" class="form-input check-value" placeholder="Koju pjesmu želite čuti?" />
+			              <button type="button" name="destination-submit" class="form-submit btn btn-special">Pošalji</button>
+			            </div>
+          			</form>
+						</div>
+				</div>
+			</div>
+		</div>
+		<!-- end:header-top -->
+		<div id="fh5co-services">
+			<div class="container">
+				
+				<ul class="nav nav-tabs" role="tablist">
+			    <li role="presentation" class="active"><a href="#ponedjeljak" aria-controls="home" role="tab" data-toggle="tab">Ponedjeljak</a></li>
+			    <li role="presentation"><a href="#utorak" aria-controls="profile" role="tab" data-toggle="tab">Utorak</a></li>
+			    <li role="presentation"><a href="#srijeda" aria-controls="messages" role="tab" data-toggle="tab">Srijeda</a></li>
+			    <li role="presentation"><a href="#cetvrtak" aria-controls="settings" role="tab" data-toggle="tab">Četvrtak</a></li>
+			    <li role="presentation"><a href="#petak" aria-controls="settings" role="tab" data-toggle="tab">Petak</a></li>
+			    <li role="presentation"><a href="#subota" aria-controls="settings" role="tab" data-toggle="tab">Subota</a></li>
+			    <li role="presentation"><a href="#nedjelja" aria-controls="settings" role="tab" data-toggle="tab">Nedjelja</a></li>
+			  </ul>
+
+			  <div class="tab-content">
+			    <div role="tabpanel" class="tab-pane fade in active" id="ponedjeljak">
+			    	<div class="program">
+			    		<div class="naslov">
+			    			<h3>Ponedjeljak</h3>
+			    			<span>$datum()</span>
+			    		</div>
+			    	</div>
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="utorak">
+			    	Ovo je utorak
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="srijeda">
+			    	Ovo je srijeda
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="cetvrtak">
+			    	Ovo je cetvrtak
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="petak">
+			    	Ovo je petak
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="subota">
+			    	Ovo je subota
+			    </div>
+			    <div role="tabpanel" class="tab-pane fade" id="nedjelja">
+			    	Ovo je nedjelja
+			    </div>
+			  </div>
+
+			</div>
+		</div>
+		<!-- END fh5co-services-section -->
+		
+		<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-xs-12">
+							<div class="kontakt">
+								<li><i class="icon-microphone"></i>Radio TNT</li>
+								<li><i class="icon-location2"></i>Gaj 18</li>
+								<li><i class="icon-dashboard"></i>72270, Travnik</li>
+							</div>
+						</div>
+						<div class="col-md-4 col-xs-12">
+							<p class="fh5co-social-icons">
+								<a href="#"><i class="icon-twitter2"></i></a>
+								<a href="#"><i class="icon-facebook2"></i></a>
+								<a href="#"><i class="icon-instagram"></i></a>
+								<a href="#"><i class="icon-dribbble2"></i></a>
+								<a href="#"><i class="icon-youtube"></i></a>
+							</p>
+							<p>Copyright 2016 TNT radio. All Rights Reserved. <br>Design by <i class="icon-heart3"></i> by <a href="http://insideo.eu/" target="_blank">Insideo</a></p>
+						</div>
+						<div class="col-md-4 col-xs-12">
+							<div class="nwl">
+								<h3>Prijavite se na newsletter</h3>
+								<form>
+									<input type="text" name="name" placeholder="Unesite e-mail adresu">
+									<button>Pošalji</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<div id="foot">
+			<div class="container">
+				<div class="lijevo">
+					© TNT Radio 2016
+				</div>
+				<div class="desno">
+					<li><a href="#">Impressum</li>
+					<li><a href="#">Oglašavanje</li>
+					<li><a href="#">Cjenik</li>
+					<li><a href="#">Kontakt</li>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- jQuery -->
+
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
